d342e563eab43fed637105ef792a5b26a00b07ad
Luís Guilherme Ferreira Bizzi <40295389+lgbizzi@users.noreply.github.com>
2021-05-03 11:48:43 -0300

    Qtd. Itens Armazenados por Cod. Itens
    Consulta para extrair a Quantidade de Itens armazenados por Código de Itens.
    
    Query to show stored items quantity for each item.

---
+++ HANA/Quantidade_de_Itens_Armazenados_por_Codigo_de_Item.sql
@@ -0,0 +1,11 @@
+SELECT 
+	T0."ItemCode" AS "Código do Item",
+	T0."ItemName" AS "Descricao do Item",
+	T2."WhsName" AS "Depósito",
+	T1."OnHand" AS "Quantidade no Estoque"
+FROM
+	OITM T0											--Tabela dos Cadastros de Itens
+	JOIN OITW T1 ON T0."ItemCode" = T1."ItemCode"	--Tabela das Quantidades de Itens em cada Depósito
+	JOIN OWHS T2 ON T1."WhsCode" = T2."WhsCode"		--Tabela dos Cadastros de Depósitos
+WHERE
+	T0."ItemCode" = '[%0]'							--Caso queira procurar por Código de Item
